122ee18d2d9e5481e758bd3cd5817c4206171730
sravyasripada <sravyasripada4@gmail.com>
2022-03-03 08:52:20 +0530

    Created Skeleton of Home Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-
+import './App.css'
 
 function App() {
   return (
-    <div >
+    <div>
+      <div >
     Phone Directory
     </div>
+     <button >Add</button>  
+     <span>Name</span><br/>
+     <span>Phone</span>
+        </div>
   );
 }
 
