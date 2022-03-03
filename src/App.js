5f3b2d94002d87c10ce6510db5cab1c42519b04e
sravyasripada <sravyasripada4@gmail.com>
2022-03-03 09:12:13 +0530

    Changed App to class base component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import './App.css'
 
-function App() {
-  return (
-    <div>
+import React,{Component} from 'react';
+class App extends Component{
+  render(){
+    return (
       <div >
-    Phone Directory
-    </div>
-     <button >Add</button>  
-     <span>Name</span><br/>
-     <span>Phone</span>
-        </div>
-  );
+         <div >
+      Phone Directory
+      </div>
+       <button >Add</button>  
+       <span>Name</span><br/>
+        <span>Phone</span>     
+       
+          </div>
+    );
+  }
+ 
 }
 
 export default App;
