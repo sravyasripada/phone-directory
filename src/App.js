f261dade4f5feb3411422baddc317e44cdaf71d3
sravyasripada <sravyasripada4@gmail.com>
2022-03-03 10:01:38 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -1,19 +1,59 @@
 
 import React,{Component} from 'react';
+import './App.css';
+import Header from './Header';
+
 class App extends Component{
-  render(){
+  
+  render(){  
+    let subscribers=[
+      { id:1,
+        name:'Sravya',
+        phone:'9845672398'      
+
+      },
+      { id:2,
+        name:'Bhanu',
+        phone:'9874521365'      
+
+      },
+      { id:3,
+        name:'Lucky',
+        phone:'9854624125'      
+
+      },
+      { id:4,
+        name:'Krish',
+        phone:'9856324718'      
+
+      },
+      { id:5,
+        name:'Ram',
+        phone:'9758324962'      
+
+      },
+    ]  
     return (
-      <div >
-         <div >
-      Phone Directory
-      </div>
-       <button >Add</button>  
-       <span>Name</span><br/>
-        <span>Phone</span>     
-       
+      <div>
+        <Header/>
+        <div className='component-body-container'>
+      <button className='custom-btn add-btn'>Add</button>
+      <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
           </div>
-    );
-  }
+    </div>
+    {
+      subscribers.map(sub =>{
+        return <div key={sub.id} className="grid-container">
+          <span className='grid-item'>{sub.name}</span>
+          <span className='grid-item'>{sub.phone}</span>
+          </div>
+      })
+    }
+           </div> 
+            );
+      }
  
 }
 
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header=function(){
+    return(
+        <div style={{textAlign:'center',textTransform:'uppercase',padding:20,backgroundColor:'black',color:'white'}}>         
+    Phone Directory  
+    </div> 
+    )
+     
+}
+export default Header;
